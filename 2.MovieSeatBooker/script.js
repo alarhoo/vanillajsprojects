646d4891a386aa4b57f5460524656d7d3a681107
manja1227 <manjunatha1227@gmail.com>
2022-04-06 23:58:27 +0530

    Movie Seat Booker App

---
+++ 2.MovieSeatBooker/script.js
@@ -0,0 +1,59 @@
+// @ts-nocheck
+// (function () {
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("selectedMovie");
+
+let ticketPrice = +movieSelect.value;
+populateUI();
+
+function updateSelectedCountAndTotal() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  // console.log(seatsIndex);
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  setMovieData(movieSelect.selectedIndex, movieSelect.value);
+}
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+    e.target.classList.toggle("selected");
+  }
+  updateSelectedCountAndTotal();
+});
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCountAndTotal();
+});
+// })();
+
+updateSelectedCountAndTotal();
